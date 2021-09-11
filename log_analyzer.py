7d6e88168bf8122a30297e6c1c033d87c4f024fe
farhanahmed3223 <farhanahmed3223@proton.me>
2021-09-11 14:22:00 +0500

    color output by severity level

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 import sys
 import time
+import re
+
+COLORS = {
+    'ERROR':    '\033[91m',
+    'CRITICAL': '\033[91m',
+    'WARNING':  '\033[93m',
+    'INFO':     '\033[92m',
+    'DEBUG':    '\033[94m',
+    'RESET':    '\033[0m',
+}
+
+def colorize(line):
+    for level, color in COLORS.items():
+        if level in line.upper():
+            return f"{color}{line}{COLORS['RESET']}"
+    return line
 
 def tail(filename):
-    """Tail a log file and print new lines."""
     with open(filename, 'r') as f:
-        f.seek(0, 2)  # go to end
+        f.seek(0, 2)
         while True:
             line = f.readline()
             if line:
-                print(line, end='')
+                print(colorize(line.rstrip()))
             else:
                 time.sleep(0.1)
 
